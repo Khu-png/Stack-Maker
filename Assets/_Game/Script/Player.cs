09bbe2acccbbef7d71e97953c27eded3aed8e1b5
Khuse <khanh220304@gmail.com>
2026-03-26 21:38:09 +0700

    Fixing

--- Assets/_Game/Script/Player.cs
+++ Assets/_Game/Script/Player.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Numerics;
-using System.Security;
-using Unity.VisualScripting;
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
